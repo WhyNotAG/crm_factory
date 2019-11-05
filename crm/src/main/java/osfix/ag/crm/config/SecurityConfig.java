eaef8c6d242a09496e50e856e3c1234783c86450
Aleksandr Golubkin <golubkin1998@mail.ru>
2019-11-05 21:33:02 +0300

    minor security updates

--- crm/src/main/java/osfix/ag/crm/config/SecurityConfig.java
+++ crm/src/main/java/osfix/ag/crm/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package osfix.ag.crm.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,7 +12,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
