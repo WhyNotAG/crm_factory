baa090856c7d86eb23e51f5f90c69445c4e1dc95
Aleksandr Golubkin <golubkin1998@mail.ru>
2019-11-06 23:37:10 +0300

    minor updates

--- crm/src/main/java/osfix/ag/crm/config/WebMvcConfig.java
+++ crm/src/main/java/osfix/ag/crm/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
