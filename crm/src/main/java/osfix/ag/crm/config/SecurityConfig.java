692981ebd06947b78311aafb2d4875620e385e50
Aleksandr Golubkin <golubkin1998@mail.ru>
2019-11-07 00:57:36 +0300

    update security

--- crm/src/main/java/osfix/ag/crm/config/SecurityConfig.java
+++ crm/src/main/java/osfix/ag/crm/config/SecurityConfig.java
@@ -21,7 +21,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults());
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf()
+                .disable()
+                .formLogin()
+                .disable()
+                .httpBasic()
+                .disable();
     }
 
 
