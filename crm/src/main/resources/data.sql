8845f4a9047a6d1315db93c38d0ff514ae551797
Aleksandr Golubkin <golubkin1998@mail.ru>
2019-12-02 00:10:13 +0300

    add db properties

--- crm/src/main/resources/data.sql
+++ crm/src/main/resources/data.sql
@@ -1,6 +1,9 @@
-INSERT INTO roles (id, name) values (1, 'ADMIN');
-INSERT INTO roles (id, name) values (2, 'USER')
+INSERT INTO roles (id, name) values (1, 'ROLE_ADMIN');
+INSERT INTO roles (id, name) values (2, 'ROLE_MANAGER');
+INSERT INTO roles (id, name) values (3, 'ROLE_WORKSHOP');
 
 
-INSERT INTO users (id, username, password) values (2, 'user', '$2a$10$MGzsOEq6zHF1r7NcDVBjmumn7HfVFBXsrVMSONuWuuUXCYX71ha/K')
 
+INSERT INTO users (id, username, password) values (1, 'test', '$2a$10$MGzsOEq6zHF1r7NcDVBjmumn7HfVFBXsrVMSONuWuuUXCYX71ha');
+
+INSERT INTO user_roles(user_id, role_id) values (1,1);
