3bf79919dd974784b63d662174e9269930b62e85
Aleksandr Golubkin <golubkin1998@mail.ru>
2019-12-07 18:04:32 +0300

    update data.sql to deploy

--- crm/src/main/resources/data.sql
+++ crm/src/main/resources/data.sql
@@ -1,9 +1,6 @@
--- INSERT INTO roles (id, name) values (1, 'ROLE_ADMIN');
--- INSERT INTO roles (id, name) values (2, 'ROLE_MANAGER');
--- INSERT INTO roles (id, name) values (3, 'ROLE_WORKSHOP');
---
---
---
--- INSERT INTO users (id, username, password) values (1, 'test', '$2a$10$MGzsOEq6zHF1r7NcDVBjmumn7HfVFBXsrVMSONuWuuUXCYX71ha');
---
--- INSERT INTO user_roles(user_id, role_id) values (1,1);
+INSERT INTO roles (id, name) values (2, 'ROLE_MANAGER');
+INSERT INTO roles (id, name) values (3, 'ROLE_WORKSHOP');
+
+INSERT INTO users (id, username, password) values (1, 'test', '$2a$10$MGzsOEq6zHF1r7NcDVBjmumn7HfVFBXsrVMSONuWuuUXCYX71ha');
+
+INSERT INTO user_roles(user_id, role_id) values (1,1);
