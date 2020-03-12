0ac945c4af0c5362955c67bf7efa5f18bd2cdd11
Aleksandr Golubkin <golubkin1998@mail.ru>
2020-03-12 22:02:23 +0300

    minor updates

--- crm/src/main/java/osfix/ag/crm/repo/manager/HistoryRepo.java
+++ crm/src/main/java/osfix/ag/crm/repo/manager/HistoryRepo.java
@@ -1,8 +1,10 @@
 package osfix.ag.crm.repo.manager;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import osfix.ag.crm.domain.manager.History;
 
+@Repository
 public interface HistoryRepo extends JpaRepository<History, Long> {
 
 }
