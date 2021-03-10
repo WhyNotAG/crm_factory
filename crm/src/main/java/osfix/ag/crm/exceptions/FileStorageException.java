54879b2794cd98f0459e57c46f444e4f7ba31e7e
WhyNotAG <golubkin1998@mail.ru>
2021-03-10 17:16:47 +0300

    add new multipart controller

---
+++ crm/src/main/java/osfix/ag/crm/exceptions/FileStorageException.java
@@ -0,0 +1,11 @@
+package osfix.ag.crm.exceptions;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
